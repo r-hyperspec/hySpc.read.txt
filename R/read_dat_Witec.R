a6a077dacebd52f646b1fd7d342bedde1eb760a5
Claudia Beleites <Claudia.Beleites@chemometrix.gmbh>
2020-07-05 20:53:28 +0200

    Refactor read_txt_Witec.R into smaller files
    - separate file for each format
    - separate file for helper functions

---
+++ R/read_dat_Witec.R
@@ -0,0 +1,81 @@
+
+#' @rdname read.txt.Witec
+#' @param filex filename wavelength axis file
+#' @param filey filename intensity file
+#' @export
+read.dat.Witec <- function(filex = stop("filename or connection needed"),
+                           filey = sub("-x", "-y", filex),
+                           points.per.line = NULL,
+                           lines.per.image = NULL,
+                           type = c("single", "map"),
+                           encoding = "unknown",
+                           ...,
+                           quiet = hy.getOption("debuglevel") < 1L) {
+  ## check valid data connection
+  .check.con(filex = filex, filey = filey)
+
+  ## check valid input
+  type <- .check.valid(
+    type = type, points.per.line = points.per.line,
+    lines.per.image = lines.per.image
+  )
+
+  ## read data
+  wl <- scan(file = filex, ..., quiet = quiet, encoding = encoding)
+  spc <- scan(file = filey, ..., quiet = quiet, encoding = encoding)
+
+  dim(spc) <- c(length(wl), length(spc) / length(wl))
+
+  spc <- new("hyperSpec", wavelength = wl, spc = t(spc))
+
+  ## add map information
+  if (type == "map") {
+    spc <- .parse.xy(spc = spc, points.per.line = points.per.line, lines.per.image = lines.per.image)
+  }
+
+  ## consistent file import behaviour across import functions
+  .fileio.optional(spc, filey)
+}
+
+test(read.dat.Witec) <- function() {
+  context("read.dat.Witec")
+
+  test_that("-y file guessing", {
+    skip("TODO: adapt to new package")
+    spc <- read.dat.Witec("fileio/txt.Witec/Witec-timeseries-x.dat")
+    expect_known_hash(spc, "9562f59323")
+  })
+
+  test_that("encoding", {
+    skip("TODO: adapt to new package")
+    spc <- read.dat.Witec("fileio/txt.Witec/Witec-timeseries-x.dat", encoding = "ascii")
+    expect_known_hash(spc, "9562f59323")
+  })
+
+  test_that("Time series", {
+    skip("TODO: adapt to new package")
+    spc <- read.dat.Witec("fileio/txt.Witec/Witec-timeseries-x.dat", "fileio/txt.Witec/Witec-timeseries-y.dat")
+    expect_known_hash(spc, "9562f59323")
+  })
+
+  test_that("Map: .dat does not have spatial information", {
+    skip("TODO: adapt to new package")
+    spc <- read.dat.Witec("fileio/txt.Witec/Witec-Map-x.dat", "fileio/txt.Witec/Witec-Map-y.dat")
+    expect_null(spc$x)
+    expect_null(spc$y)
+    expect_known_hash(spc, "8a7ed06b0b")
+  })
+
+  test_that("Map", {
+    skip("TODO: adapt to new package")
+    expect_warning(read.dat.Witec("fileio/txt.Witec/Witec-Map-x.dat", "fileio/txt.Witec/Witec-Map-y.dat",
+      points.per.line = 5, lines.per.image = 5
+    ))
+
+    spc <- read.dat.Witec("fileio/txt.Witec/Witec-Map-x.dat", "fileio/txt.Witec/Witec-Map-y.dat",
+      type = "map", points.per.line = 5, lines.per.image = 5
+    )
+    expect_known_hash(spc, "3d6339675b")
+  })
+}
+
