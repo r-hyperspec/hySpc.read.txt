5515214aed2118eb7f2c7007b11daa7ff5b61d3a
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-08-11 19:44:22 -0500

    Remove superfluos code for bechmarking

--- R/microbenchmark.R
+++ R/microbenchmark.R
@@ -1,29 +1,20 @@
-# load the microbenchmark library
+# Load the microbenchmark library
 library(microbenchmark)
 
-# io_function_1
+# Set file to be tested
+file <- "/Users/erickoduniyi/Documents/Projects/open-source/hyperspec/hySpc.read.txt/tests/testthat/Witec_TrueMatch.txt"
 
-# io_function_2
+# Switch between io packages
+# Calculate performance with microbenchmark
+res <- microbenchmark(
 
-# io_function_3
+  # read.ini
+  read.ini(file)
 
-# Aggregate benchmarking scores
-scores <- cbind("", ..., "")
+  #
 
+)
 
-# Figure out what I want to test
-# test_io(file, function_name)
-test_io <- function(file, function_name) {
+# Release data frame
+res
 
-  # Switch between io packages
-  # Calculate performance with io package
-  # Output report of performance to a data frame
-
-  # Create data frame to store the benchmarking results
-  bm_df <- data.frame(package_name = 1, score = 1)
-  bm_df$package_name <- function_name
-  bm_df$score <- "Put report value here"
-
-  # Release data frame
-  bm_df
-}
