6f46bda944c34440c0b4b2ba39276005759fda1a
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-08-11 21:00:02 -0500

    Add additional file io options

--- R/microbenchmark.R
+++ R/microbenchmark.R
@@ -8,10 +8,36 @@ file <- "/Users/erickoduniyi/Documents/Projects/open-source/hyperspec/hySpc.read
 # Calculate performance with microbenchmark
 res <- microbenchmark(
   read.ini(file),
-  read.table(file, colClasses),
+  read.table ("txt.t/Triazine 5_31.txt", header = TRUE, dec = ",", sep = "\t"),
   read.table(file, header = FALSE, sep="", dec="."),
-  readtext(file)
+  readtext(file),
+  scan.txt.Witec(file),
+  readLines(file, encoding = encoding)
 )
+
+# Throw the Kitchen Sink at it
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_full.txt",  type = "map", hdr.label = TRUE, hdr.units = TRUE)
+#
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_label.txt", type = "map", hdr.label = TRUE, hdr.units = FALSE)
+#
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_unit.txt",  type = "map", hdr.label = FALSE, hdr.units = TRUE)
+#
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_unit.txt",
+#                type = "map", hdr.label = FALSE, hdr.units = TRUE,
+#                points.per.line = 5
+# )
+#
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_no.txt", type = "map", hdr.label = FALSE, hdr.units = FALSE)
+#
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_no.txt",
+#                type = "map", hdr.label = FALSE, hdr.units = FALSE,
+#                lines.per.image = 5
+# )
+#
+# read.txt.Witec("fileio/txt.Witec/Witec-Map_no.txt",
+#                type = "map", hdr.label = FALSE, hdr.units = FALSE,
+#                points.per.line = 5, lines.per.image = 5
+# )
 
 # Release data
 res
