e25445c4381625b3a2ead850812fbb321e0b764b
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-08-13 16:34:44 -0500

    Remove the read.txt.Witec comments

--- R/microbenchmark.R
+++ R/microbenchmark.R
@@ -3,10 +3,9 @@ library(hyperSpec)
 library(microbenchmark)
 library(readtext)
 
-
 # Set file to be tested
 file <- "/Users/erickoduniyi/Documents/Projects/open-source/hyperspec/hySpc.read.txt/tests/testthat/Witec_TrueMatch.txt"
-res <- 10
+
 # Switch between io packages
 # Calculate performance with microbenchmark
 microbenchmark(
@@ -16,30 +15,4 @@ microbenchmark(
   readtext(file),
   default = read.ini(file)
 )
-
-# Throw the Kitchen Sink at it
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_full.txt",  type = "map", hdr.label = TRUE, hdr.units = TRUE)
-#
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_label.txt", type = "map", hdr.label = TRUE, hdr.units = FALSE)
-#
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_unit.txt",  type = "map", hdr.label = FALSE, hdr.units = TRUE)
-#
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_unit.txt",
-#                type = "map", hdr.label = FALSE, hdr.units = TRUE,
-#                points.per.line = 5
-# )
-#
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_no.txt", type = "map", hdr.label = FALSE, hdr.units = FALSE)
-#
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_no.txt",
-#                type = "map", hdr.label = FALSE, hdr.units = FALSE,
-#                lines.per.image = 5
-# )
-#
-# read.txt.Witec("fileio/txt.Witec/Witec-Map_no.txt",
-#                type = "map", hdr.label = FALSE, hdr.units = FALSE,
-#                points.per.line = 5, lines.per.image = 5
-# )
-
-# Release data
 
