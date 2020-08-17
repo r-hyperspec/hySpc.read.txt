bc3e643982633933276a959a403fd70b2c653f1b
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-08-17 18:09:54 -0500

    Removed local information from script

--- R/microbenchmark.R
+++ R/microbenchmark.R
@@ -4,7 +4,7 @@ library(microbenchmark)
 # library(readtext)
 
 # Set file to be tested
-file <- "/Users/erickoduniyi/Documents/Projects/open-source/hyperspec/hySpc.read.txt/tests/testthat/Witec_TrueMatch.txt"
+file <- "path/to/the/testthat/Witec_TrueMatch.txt"
 
 # Switch between io packages
 # Calculate performance with microbenchmark
