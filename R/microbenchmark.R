2b1e1f4f1c214cbde88d04eb083a17ac435b8b50
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-08-17 17:57:32 -0500

    Comment out `readtext` to avoid triggering the check processes

--- R/microbenchmark.R
+++ R/microbenchmark.R
@@ -1,7 +1,7 @@
 # Load the microbenchmark library
 library(hyperSpec)
 library(microbenchmark)
-library(readtext)
+# library(readtext)
 
 # Set file to be tested
 file <- "/Users/erickoduniyi/Documents/Projects/open-source/hyperspec/hySpc.read.txt/tests/testthat/Witec_TrueMatch.txt"
@@ -12,7 +12,7 @@ microbenchmark(
   read.table(file, header = TRUE, dec = ",", sep = "\t"),
   readLines(file),
   read.ini(file),
-  readtext(file),
+  # readtext(file),
   default = read.ini(file)
 )
 
