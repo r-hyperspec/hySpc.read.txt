7a0abb582eeeae7403994c2eaf3fc143aa196b43
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-30 15:15:23 +0300

    Update read_txt_Horiba.R
    Update style and documentation.

--- R/read_txt_Horiba.R
+++ R/read_txt_Horiba.R
@@ -1,22 +1,40 @@
-#' Read ASCII (`.txt`) files exported by Horiba's Labspec software (LabRAM spectrometers)
+# Function -------------------------------------------------------------------
+
+#' Read Horiba Labspec files (ASCII/txt)
 #'
-#' `read_txt_Horiba_xy` reads maps, i.e. `.txt` files where the first two columns give x and y coordinates.
+#' Read ASCII (`.txt`) files exported by Horiba's Labspec software
+#' (LabRAM spectrometers).
 #'
-#' @title Import Horiba Labspec exported ASCII files
-#' @param file connection (file name and path) to the `.txt` file
+#' - `read_txt_Horiba()`
+#' - `read_txt_Horiba_xy()` reads maps, i.e. `.txt` files where the first two
+#'    columns give x and y coordinates.
+#' - `read_txt_Horiba_t()` reads time series, i.e. `.txt` files with the time
+#'    in the first column.
+#'
+#' @rdname read_txt_Horiba
+#'
+#' @param file Path of connection to `.txt` file.
 #' @param cols,header,sep,row.names,check.names,... further parameters are
 #'        handed over to [hyperSpec::read_txt_wide()]
-#' @rdname read_txt_Horiba
+#'
+#' @return `hyperSpec` object.
+#'
 #' @author C. Beleites
-#' @return hyperSpec object
+#'
 #' @concept io
+#'
 #' @export
-read_txt_Horiba <- function(file, cols = c(
+#'
+read_txt_Horiba <- function(file,
+                            cols = c(
                               spc = "I / a.u.",
                               .wavelength = expression(Delta * tilde(nu) / cm^-1)
                             ),
-                            header = TRUE, sep = "\t", row.names = NULL,
-                            check.names = FALSE, ...) {
+                            header = TRUE,
+                            sep = "\t",
+                            row.names = NULL,
+                            check.names = FALSE,
+                            ...) {
   spc <- read_txt_wide(file,
     cols = cols,
     header = header, sep = sep, row.names = row.names,
@@ -29,11 +47,11 @@ read_txt_Horiba <- function(file, cols = c(
   spc
 }
 
+# Function -------------------------------------------------------------------
+
 #' @rdname read_txt_Horiba
 #' @export
-#'
-#' @concept io
-#'
+
 read_txt_Horiba_xy <- function(file, ...) {
   read_txt_Horiba(
     file = file,
@@ -47,15 +65,16 @@ read_txt_Horiba_xy <- function(file, ...) {
   )
 }
 
-#' `read_txt_Horiba_t`  reads time series, i.e. .txt files with the time in the first column
+# Function -------------------------------------------------------------------
+
 #' @rdname read_txt_Horiba
 #' @export
-#'
-#' @concept io
-#'
+
+
 read_txt_Horiba_t <- function(file, header = TRUE, sep = "\t", row.names = NULL,
                               check.names = FALSE, ...) {
-  read_txt_Horiba(file,
+  read_txt_Horiba(
+    file,
     cols = c(
       t = "t / s",
       spc = "I / a.u.",
@@ -64,3 +83,7 @@ read_txt_Horiba_t <- function(file, header = TRUE, sep = "\t", row.names = NULL,
     ...
   )
 }
+
+# Unit tests -----------------------------------------------------------------
+
+# FIXME: add unit tests
