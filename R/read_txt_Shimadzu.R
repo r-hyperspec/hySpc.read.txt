e71e78b0e042c306b65ab776ec4656e3e3a26b01
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-08-06 20:52:41 +0300

    Update read_txt_Shimadzu.R

--- R/read_txt_Shimadzu.R
+++ R/read_txt_Shimadzu.R
@@ -225,8 +225,15 @@ read_txt_Shimadzu <- function(filename, encoding = "", quiet = TRUE) {
 
 hySpc.testthat::test(read_txt_Shimadzu) <- function() {
   context("read_txt_Shimadzu")
-  gc <- system.file("extdata/txt.Shimadzu/", "GCxGC-qMS.txt", package = "hySpc.read.txt")
-  spc <- read_txt_Shimadzu(gc)
+
+  filename <- system.file(
+    "extdata",
+    "txt.Shimadzu/GCxGC-qMS.txt",
+    package = "hySpc.read.txt"
+  )
+
+  spc <- read_txt_Shimadzu(filename)
+
   test_that("Shimadzu .txt",{
     expect_equal(length(spc), 3)
 
