78030f0fa81bb24286fd5ef7b67d34a80016be11
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-08-09 13:10:14 +0300

    Add `msg_open_issue_to_contribute_file()`

--- R/messages.R
+++ R/messages.R
@@ -9,6 +9,16 @@ msg_open_issue_and_add_file <- function(pkg = "hySpc.read.txt") {
   )
 }
 
+msg_open_issue_to_contribute_file <- function(pkg = "hySpc.read.txt") {
+  paste0(
+    "To help improving '", pkg, "', ",
+    "please consider submitting your spectra file as an example ",
+    "(possibly included in a ZIP archive) by opening an issue at \n",
+    packageDescription(pkg)$BugReports
+  )
+}
+
+
 # Unit tests -----------------------------------------------------------------
 
 hySpc.testthat::test(msg_open_issue_and_add_file) <- function() {
@@ -17,6 +27,14 @@ hySpc.testthat::test(msg_open_issue_and_add_file) <- function() {
   test_that("msg_open_issue_and_add_file() works", {
 
     expect_silent(msg <- msg_open_issue_and_add_file())
+    expect_match(msg, "hySpc.read.txt")
+    expect_match(msg, "r-hyperspec")
+    expect_match(msg, "file")
+  })
+
+  test_that("msg_open_issue_to_contribute_file() works", {
+
+    expect_silent(msg <- msg_open_issue_to_contribute_file())
     expect_match(msg, "hySpc.read.txt")
     expect_match(msg, "r-hyperspec")
     expect_match(msg, "file")
