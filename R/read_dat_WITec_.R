fdeabdbb75da3fb85d529df01e81b0990122d966
Sang T. Truong <sttruong@stanford.edu>
2021-08-12 12:05:36 -0400

    unit test for read dat

--- R/read_dat_WITec_.R
+++ R/read_dat_WITec_.R
@@ -42,86 +42,57 @@ read_dat_WITec <- function(filex = stop("filename or connection needed"),
 # Unit tests -----------------------------------------------------------------
 
 hySpc.testthat::test(read_dat_WITec) <- function() {
-  local_edition(2)
-
   context("read_dat_WITec")
 
-  tmpdir <- paste0(tempdir(), "/test_Witec_dat")
-  on.exit(unlink(tmpdir, force = TRUE))
+  local_edition(3)
 
-  untar("testfiles_Witec.tar.gz",
-    files = c(
-      "Witec-Map-x.dat",
-      "Witec-Map-y.dat",
-      "Witec-timeseries-x.dat",
-      "Witec-timeseries-y.dat"
-    ),
-    exdir = tmpdir
+  filename <- system.file(
+    "extdata",
+    "txt.Witec/Witec-timeseries-x.dat",
+    package = "hySpc.read.txt"
   )
 
-  test_that("-y file guessing", {
-    spc <- read_dat_WITec(paste0(tmpdir, "/Witec-timeseries-x.dat"))
-    spc$filename <- gsub("^.*/", "", spc$filename)
-    expect_known_hash(spc, "1977fe5997")
+  expect_silent(spc <- read_dat_WITec(filename))
+
+  n_wl <- nwl(spc)
+  n_rows <- nrow(spc)
+  n_clos <- ncol(spc)
+
+  test_that("WITec .dat: hyperSpec obj. dimensions are correct", {
+    expect_equal(n_wl, 1024)
+    expect_equal(n_rows, 100)
+    expect_equal(n_clos, 2)
   })
 
-  test_that("encoding", {
-    spc <- read_dat_WITec(
-      paste0(tmpdir, "/Witec-timeseries-x.dat"),
-      encoding = "ascii"
-    )
-    spc$filename <- gsub("^.*/", "", spc$filename)
-
-    expect_known_hash(spc, "1977fe5997")
+  test_that("WITec .dat: extra data are correct", {
+    # @data colnames
+    expect_equal(colnames(spc), c("spc", "filename"))
   })
 
-  test_that("Time series", {
-    spc <- read_dat_WITec(
-      paste0(tmpdir, "/Witec-timeseries-x.dat"),
-      paste0(tmpdir, "/Witec-timeseries-y.dat")
-    )
-    spc$filename <- gsub("^.*/", "", spc$filename)
-
-    expect_known_hash(spc, "1977fe5997")
+  test_that("WITec .dat: labels are correct", {
+    expect_equal(spc@label$.wavelength, NULL)
+    expect_equal(spc@label$spc, NULL)
+    expect_equal(spc@label$filename, "filename")
   })
 
-  test_that("Map: .dat does not have spatial information", {
-    spc <- read_dat_WITec(
-      paste0(tmpdir, "/Witec-Map-x.dat"),
-      paste0(tmpdir, "/Witec-Map-y.dat")
-    )
-    expect_null(spc$x)
-    expect_null(spc$y)
-    spc$filename <- gsub("^.*/", "", spc$filename)
+  test_that("WITec .dat: spectra are correct", {
+    # Dimensions of spectra matrix (@data$spc)
+    expect_equal(dim(spc@data$spc), c(100, 1024))
 
-    expect_known_hash(spc, "b523735004")
+    # Column names of spectra matrix
+    expect_equal(colnames(spc@data$spc)[1], "87.7367")
+    expect_equal(colnames(spc@data$spc)[10], "117.333")
+    expect_equal(colnames(spc@data$spc)[n_wl], "2821.31") # last name
+
+    # Values of spectra matrix
+    expect_equal(unname(spc@data$spc[1, 1]), 995)
+    expect_equal(unname(spc@data$spc[2, 10]), 1002)
+    expect_equal(unname(spc@data$spc[n_rows, n_wl]), 985) # last spc value
   })
 
-  test_that("Map", {
-    expect_warning(
-      read_dat_WITec(
-        paste0(tmpdir, "/Witec-Map-x.dat"),
-        paste0(tmpdir, "/Witec-Map-y.dat"),
-        points.per.line = 5, lines.per.image = 5
-      ),
-      "points.per.line != 1 given for single spectrum"
-    )
-    expect_warning(
-      read_dat_WITec(
-        paste0(tmpdir, "/Witec-Map-x.dat"),
-        paste0(tmpdir, "/Witec-Map-y.dat"),
-        points.per.line = 5, lines.per.image = 5
-      ),
-      "lines.per.image != 1 are defined for single spectrum"
-    )
-
-    spc <- read_dat_WITec(
-      paste0(tmpdir, "/Witec-Map-x.dat"),
-      paste0(tmpdir, "/Witec-Map-y.dat"),
-      type = "map", points.per.line = 5, lines.per.image = 5
-    )
-    spc$filename <- gsub("^.*/", "", spc$filename)
-
-    expect_known_hash(spc, "efc28c0d45")
+  test_that("WITec .dat: wavelengths are correct", {
+    expect_equal(spc@wavelength[1], 87.736672)
+    expect_equal(spc@wavelength[10], 117.332683)
+    expect_equal(spc@wavelength[n_wl], 2821.3062)
   })
 }
